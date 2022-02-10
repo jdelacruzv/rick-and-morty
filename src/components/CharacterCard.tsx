a20024b465bf5ac1b7fff23249da572c1aee071d
Jose De La Cruz <jldlcv@gmail.com>
2022-02-10 07:02:57 -0500

    First commit

---
+++ src/components/CharacterCard.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Character } from '../types';
+import '../App.css';
+
+export type Props = {
+  character: Character;
+};
+
+export const Card: React.FC<Props> = (props) => {
+  const { character } = props;
+  return (
+    <li>
+      <h3>{character.name}</h3>
+      <img 
+        className='character-img'
+        src={character.image} 
+        alt={character.name} 
+        width={180}
+      />
+      <p>{character.status}</p>
+    </li>
+  );
+};
+
+export default Card;
---
+++ src/components/CharacterList.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Character } from '../types';
+import { Card } from './CharacterCard';
+import '../App.css';
+
+export const CharacterList = () => {
+  // useState devuelve un array e inicializa en este caso un array vacío
+  // el primer valor viene hacer como getter y el segundo como un setter
+  const [characterList, setCharacterList] = React.useState<Character[]>([]);
+
+  // useEffect permite tener disponible el ciclo de vida del componente 
+  // para ejecutar funciones en distintos momentos
+  React.useEffect(() => {
+    // función autoinvocada
+    (async () => {
+      let data = await fetch(`https://rickandmortyapi.com/api/character/`).then((res) => res.json());
+      setCharacterList(data.results);
+    })();
+  }, []);
+
+  return (
+    <>
+      <ul className="character-list">
+        {characterList.map((character) => (
+          <Card key={character.id} character={character} />
+        ))}
+      </ul>
+    </>
+  );
+};
